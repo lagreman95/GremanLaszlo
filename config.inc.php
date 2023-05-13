0d5097dd3103189ecb25fa3ed360e1ef48e8a866
Laszlo Greman <104382919+lagreman95@users.noreply.github.com>
2023-05-13 09:37:51 +0200

    Add files via upload

---
+++ config.inc.php
@@ -0,0 +1,28 @@
+
+<?php
+    $ablakcim = array(
+        'cim' => 'Turista utak.',
+    );
+    
+    $fejlec = array(
+        'cim' => 'Turista utak',
+        'motto' => 'Az ember és a természet felbonthatatlanul összekapcsolódik és közös végzethez van kötve.'
+    );
+    
+    $lablec = array(
+        'copyright' => 'Copyright '.date("Y").'.',
+        'ceg' => '<a href="https://turistautak.hu" target="_blank">Turistautak weboldal</a>'
+    );
+    
+    $oldalak = array(
+        '/' => array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+        'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galéria', 'menun' => array(1,1)),
+        'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+        'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+        'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+        'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+        'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+    );
+    
+    $hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
---
+++ fooldal.tpl.php
@@ -0,0 +1,13 @@
+<h2>Kedves Túrázók!</h2>
+<img src="images/fooldal.jpg" width="300" height="150">
+<p>A <strong>turistautak.hu</strong> Magyarország leggyorsabban fejlődő nonprofit térkép-portálja, ingyenesen hozzáférhető, igényes elektronikus térképszolgáltatásokkal.</p>
+<p>A névben vállalt turistautakon túl a teljes közúthálózat és a települések utcaszintű térképezését is végzi, és elektronikus térképek széles skáláján publikálja. Ezen túlmenően helyek sokaságát gyűjti és csoportosítja nyilvánosan elérhető adatbázisában, információkat szolgáltat, túraútvonalakat ajánl.</p>
+<p>A nagyszabású kezdeményezésen dolgozó lelkes csapat munkája összetett: GPS-es terepbejárásoktól térképrajzoláson át a fejlesztésekig mindent magunk végzünk - szabadidőben, nonprofit alapon. Interneten szerveződő közösségünk nyitott, folyamatosan várjuk a felhasználók visszajelzéseit és újabb tagok bekapcsolódását a munka bármely szintjén.</p>
+<p>Eddig összesen <strong>178856 km</strong> utat jártunk be és térképeztünk fel. Ebből <strong>26542 km</strong> jelzett turistaút.</p>
+<br>
+<iframe width="500" height="300" src="https://www.youtube.com/embed/ZFip6TkLbbI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<video width="500" height="300" controls>
+  <source src="images/video.mp4" type="video/mp4">
+</video>
+<h3>Itt megtalál bennünket:</h3>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2742004.581249725!2d15.343993887499995!3d47.844652400000015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47405b243ed6e205%3A0x95888f471d209f96!2sTurisztikai%20K%C3%B6zpont!5e0!3m2!1shu!2shu!4v1683898352768!5m2!1shu!2shu" width="450" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
